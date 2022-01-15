0c2fab54e5e4aac338fda0ddcdbcaa21f5d4c6d7
Julian Browne <julianbrowne@gmail.com>
2022-01-15 09:59:39 +0000

    modified src from word master

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    screens: {
+      xxs: '321px',
+      xs: '475px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        background: '#FFFFFF',
+        primary:
+          'hsl(231, 16%, 25%)',
+          'background-dark': 'hsl(231, 16%, 25%)',
+          'primary-dark': 'hsl(231, 16%, 92%)',
+          'yellow-500': '#FFA07A',
+          'n-green': '#8FBC8F',
+          'n-gray': '#2F4F4F',
+      },
+    },
+    neumorphismSize: {
+      xs: '0.05em',
+      sm: '0.1em',
+      default: '0.2em',
+      lg: '0.4em',
+      xl: '0.8em',
+    },
+  },
+  plugins: [require('tailwindcss-neumorphism')],
+}
